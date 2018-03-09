8e1e543eff821afe3bb16d045de82682c3262e77
Rilyaus <rilyaus.gd@gmail.com>
2018-03-09 18:00:34 +0900

    Node JS Tutorial Comp

--- app_orientdb.js
+++ app_orientdb.js
@@ -20,12 +20,55 @@ app.set('views', './views_orientdb');
 app.set('view engine', 'pug');
 
 app.get('/topic/add', (req, res) => {
-    fs.readdir('data', (err, files) => {
-        if(err) {
-            console.log(err);
-            res.status(500).send('Internal Server Error');
+    var sql = 'SELECT FROM topic';
+    db.query(sql).then( (topics) => {
+        res.render('add', {topics:topics});
+    });
+});
+
+app.post('/topic/add', (req, res) => {
+    var title = req.body.title;
+    var description = req.body.description;
+    var author = req.body.author;
+    var sql = 'INSERT INTO topic (title, description, author) VALUES(:title, :desc, :author)';
+    db.query(sql, {
+        params: {
+            title:title,
+            desc:description,
+            author:author
         }
-        res.render('add', {topics:files});
+    }).then( (results) => {
+        res.redirect('/topic/' + encodeURIComponent(results[0]['@rid']));
+    });
+})
+
+app.get('/topic/:id/edit', (req, res) => {
+    var sql = 'SELECT FROM topic';
+    var id = req.params.id;
+    db.query(sql).then( (topics) => {
+        var sql = 'SELECT FROM topic WHERE @rid=:rid';
+        db.query(sql, {params:{rid:id}}).then( (topic) => {
+            res.render('edit', {topics:topics, topic:topic[0]});
+        });
+    });
+});
+
+app.post('/topic/:id/edit', (req, res) => {
+    var sql = 'UPDATE topic SET title=:t, description=:d, author=:a WHERE @rid=:rid';
+    var id = req.params.id;
+    var title = req.body.title;
+    var desc = req.body.description;
+    var author = req.body.author;
+
+    db.query(sql, {
+        params: {
+            t:title,
+            d:desc,
+            a:author,
+            rid:id
+        }
+    }).then( (topics) => {
+        res.redirect('/topic/' + encodeURIComponent(id));
     });
 });
 
@@ -41,18 +84,6 @@ app.get(['/topic', '/topic/:id'], (req, res) => {
         } else {
             res.render('view', {topics:topics});
         }
-    });
-});
-
-app.post('/topic', (req, res) => {
-    var title = req.body.title;
-    var description = req.body.description;
-    fs.writeFile('data/' + title, description, (err) => {
-        if(err) {
-            console.log(err);
-            res.status(500).send('Internal Server Error');
-        }
-        res.redirect('/topic/' + title);
     });
 });
 
